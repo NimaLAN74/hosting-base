341230e0c6d87a777781f7466c67791d02d23a84
Nima Moslehi <nima_ms1@msn.com>
2026-01-13 13:55:41 +0100

    Fix Profile.js: add missing Link import from react-router-dom

--- lianel/dc/frontend/src/Profile.js
+++ lianel/dc/frontend/src/Profile.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PageTemplate from './PageTemplate';
 import { useKeycloak } from './KeycloakProvider';
 import './App.css';
