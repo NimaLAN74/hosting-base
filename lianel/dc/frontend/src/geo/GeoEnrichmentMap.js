f4ba7cc283747e200b55a8d46501d5ef30d490bd
Nima Moslehi <nima_ms1@msn.com>
2026-01-23 12:23:56 +0100

    Fix: Make Leaflet icon fix more defensive for build compatibility

--- lianel/dc/frontend/src/geo/GeoEnrichmentMap.js
+++ lianel/dc/frontend/src/geo/GeoEnrichmentMap.js
@@ -7,12 +7,19 @@ import './GeoEnrichmentMap.css';
 
 // Fix for default marker icons in React-Leaflet
 import L from 'leaflet';
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-});
+// Fix for webpack/build issues with default icons
+if (typeof window !== 'undefined' && L.Icon && L.Icon.Default) {
+  try {
+    delete L.Icon.Default.prototype._getIconUrl;
+  } catch (e) {
+    // Ignore if delete fails (strict mode or build optimization)
+  }
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+  });
+}
 
 // Component to fit map bounds to data
 function FitBounds({ bounds }) {
